61b8c65bd5c22cddbd408441cf974c5f332ab125
Ahmed Doud <ahmeddoud@Ahmeds-MacBook-Pro.local>
2024-05-06 19:47:28 +0400

    ofin-backend-project

---
+++ src/main/java/com/asastech/ofin/OfinApplication.java
@@ -0,0 +1,13 @@
+package com.asastech.ofin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfinApplication.class, args);
+	}
+
+}
